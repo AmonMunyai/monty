9bc64b5d7f340cb2ef0ba92f87067f9893db501e
AmonMunyai <Amonmunyai11@gmail.com>
2023-01-22 20:03:01 +0200

    starting point

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * main - monty driver code
+ * @argc: no. of arguments
+ * @argv: list of arguments
+ * Return: always 0 (on success)
+ */
+
+int  main(int argc, char *argv[])
+{
+
+	return (EXIT_SUCCESS);
+}
